5684300323855d815a117d016af3d3d1d4cf548a
Julia Kaeppel <jm4n1015@gmail.com>
2023-11-29 20:21:16 -0800

    Refactored page code to allow for splitting.

--- src/bounding_box.py
+++ src/bounding_box.py
@@ -3,17 +3,33 @@ import numpy as np
 import numpy.typing as npt
 import typing
 
+Point = npt.NDArray
+
 class BoundingBox:
     # Bounding box constructor.
     #
     # lower: The lower bounds of the bounding box.
     # upper: The upper bounds of the bounding box.
-    def __init__(self, lower: npt.NDArray, upper: npt.NDArray):
+    def __init__(self, lower: Point, upper: Point):
         if lower.shape != upper.shape:
             raise ValueError(f"Shape mismatch: lower bound has shape {lower.shape}, but upper bound has shape {upper.shape}.")
         self.lower = lower
         self.upper = upper
         self.shape = lower.shape
+    
+    # Expands the bounding box to contain the given point.
+    #
+    # point: The point to contain.
+    def fit_point(self, point: Point):
+        self.lower = np.minimum(self.lower, point)
+        self.upper = np.maximum(self.upper, point)
+
+    # Expands the bounding box to fully contain the given bounding box.
+    #
+    # box: The box to contain.
+    def fit_boxes(self, box: "BoundingBox"):
+        self.lower = np.minimum(self.lower, box.lower)
+        self.upper = np.maximum(self.upper, box.upper)
     
     # Determines whether another bounding box intersects with this one.
     #
@@ -26,7 +42,7 @@ class BoundingBox:
     # Checks if a point is contained within this bounding box.
     #
     # point: The point to check.
-    def contains_point(self, point: npt.NDArray) -> bool:
+    def contains_point(self, point: Point) -> bool:
         return all((self.lower <= point) & (point <= self.upper))
     
     # Creates a string representation of this bounding box.
--- src/page.py
+++ src/page.py
@@ -1,145 +1,48 @@
-# R-tree page data structure
+# R-tree page data structure.
 import numpy as np
 import numpy.typing as npt
 import typing
 
 from bounding_box import BoundingBox
 
+Point = npt.NDArray
+
+# R-tree page data structure.
+#
+# Individual pages do not handle splitting, since it is done in-place.
 class Page:
     # Page constructor.
     #
-    # shape: The shape of the keys stored in this page.
-    # rtol: The relative tolerance (See numpy.isclose).
-    # atol: The absolute tolerance (See numpy.isclose).
-    def __init__(self, shape: tuple[int,...]=(2,), rtol=1e-05, atol=1e-08):
+    # shape: The shape of the keys in this page.
+    # leaf: Whether this page is a leaf node. This is used to avoid checking types.
+    def __init__(self, shape: int | tuple[int,...]=(2,), leaf: bool=False):
         # Convert shape to tuple
         if isinstance(shape, int):
             self.shape = (shape,)
-        else:
-            self.shape = shape
-        self.rtol = rtol
-        self.atol = atol
+        
+        # Create bounding box and entries list
+        self.shape = shape
+        self.leaf = leaf
         self.entries = []
-        self.min = np.ndarray(shape)
-        self.max = np.ndarray(shape)
-        self.__recompute_bounds()
+        self.bounding_box = BoundingBox(np.full(shape, np.inf), np.full(shape, -np.inf))
     
-    # Inserts an entry into this page.
+    # Inserts an entry into the page.
     #
-    # key: The bounding box or position of the entry.
-    # val: The page or data associated with the entry.
-    def insert(self, key: BoundingBox | npt.NDArray, val: typing.Union["Page", typing.Any]):
-        # Check key shape
-        if key.shape != self.shape:
-            raise ValueError(f"Shape mismatch: key has shape {key.shape}, but page holds keys of shape {self.shape}.")
-        
-        # Add to entries list
-        entry = Entry(key, val)
-        self.entries.append(Entry(key, val))
+    # entry: The entry to insert. Either a point or a page, depending on whether this page is a leaf.
+    def add(self, entry: typing.Union[Point, "Page"]):
+        # Add entry
+        self.entries.append(entry)
 
         # Update bounding box
-        if entry.is_point:
-            self.min = np.minimum(self.min, key)
-            self.max = np.maximum(self.max, key)
+        if self.leaf:
+            self.bounding_box.fit_point(entry)
         else:
-            self.min = np.minimum(self.min, key.lower)
-            self.max = np.maximum(self.max, key.upper)
-    
-    # Inserts an entry into this page.
-    #
-    # key: The position or bounding box of the entry.
-    # val: The page or data associated with the entry.
-    def __setitem__(self, key: BoundingBox | npt.NDArray, val: typing.Union["Page", typing.Any]):
-        self.insert(key, val)
+            self.bounding_box.fit_box(entry)
 
-    # Deletes an entry from this page.
-    #
-    # key: The entry to delete.
-    def delete(self, key: BoundingBox | npt.NDArray):
-        is_point = isinstance(key, npt.NDArray)
-
-        # Find key in entries list
-        found = False
-        for i in range(len(self.entries)):
-            entry = self.entries[i]
-            # Compare key with entry
-            if (((is_point and entry.is_point) and entry.isclose_box(key, self.rtol, self.atol))
-                or ((not is_point and not entry.is_point) and entry.isclose_point(key, self.rtol, self.atol))):
-                found = True
-                break
-        
-        # Handle if entry wasn't found
-        if not found:
-            raise KeyError(f"Key not found in page.")
-        
-        # Remove entry
-        del a[i]
-        # Update bounding box
-        self.__recompute_bounds()
-
-    # Deletes an entry from this page.
-    #
-    # key: The entry to delete.
-    def __delitem__(self, key: npt.NDArray):
-        self.delete(key)
-    
-    # Gets the length of this page.
+    # Gets the number of entries in this page.
     def __len__(self) -> int:
         return len(self.entries)
     
     # Creates a string representation of this page.
     def __str__(self) -> str:
-        return str([entry.key for entry in self.entries])
-
-    # Recomputes this page's bounding box.
-    def __recompute_bounds(self):
-        self.__recompute_min()
-        self.__recompute_max()
-    
-    # Recomputes this page's minimum bounds.
-    def __recompute_min(self):
-        self.min.fill(np.inf)
-        for entry in self.entries:
-            if entry.is_point:
-                self.min = np.minimum(self.min, entry.key)
-            else:
-                self.min = np.minimum(self.min, entry.key.lower)
-
-    # Recomputes this page's maximum bounds.
-    def __recompute_max(self):
-        self.max.fill(-np.inf)
-        for entry in self.entries:
-            if entry.is_point:
-                self.max = np.maximum(self.max, entry.key)
-            else:
-                self.max = np.maximum(self.max, entry.key.upper)
-
-# Page entry.
-class Entry:
-    # Entry constructor.
-    #
-    # key: The bounding box or position of this entry.
-    # val: The page or data held by this entry.
-    def __init__(self, key: BoundingBox | npt.NDArray, val: Page | typing.Any):
-        self.is_point = not isinstance(key, BoundingBox)
-        self.is_leaf = not isinstance(val, Page)
-        self.key = key
-        self.val = val
-
-    # Checks whether a point is equal to this entry within a tolerance.
-    #
-    # other: The point to check against.
-    # rtol: The relative tolerance (See numpy.isclose).
-    # atol: The absolute tolerance (See numpy.isclose).
-    def isclose_point(self, other: npt.NDArray, rtol: float=1e-05, atol: float=1e-08) -> bool:
-        return np.isclose(self.key, other, rtol, atol)
-
-    # Checks whether a bounding box is equal to this entry within a tolerance.
-    #
-    # other: The bounding box to check against.
-    # rtol: The relative tolerance (See numpy.isclose).
-    # atol: The absolute tolerance (See numpy.isclose).
-    def isclose_box(self, other: BoundingBox, rtol: float=1e-05, atol: float=1e-08) -> bool:
-        return (np.isclose(self.key.lower, other.lower, rtol, atol)
-            and np.isclose(self.key.upper, other.upper, rtol, atol))
-    
+        return f"Page({self.bounding_box}: {self.entries})"
