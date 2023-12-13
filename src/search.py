9ce8a2db7e68b2522c9289589fcea9161a68fe6e
Julia Kaeppel <jm4n1015@gmail.com>
2023-12-12 17:07:08 -0800

    Added the option to disable bounding boxes in show_search.

--- src/search.py
+++ src/search.py
@@ -9,15 +9,16 @@ from r_tree import R_Tree
 
 Point = npt.NDArray
 
-def show_search(rt: R_Tree, radius: float, origin: Point):
+def show_search(rt: R_Tree, radius: float, origin: Point, draw_all_boxes: bool=True, draw_searched_boxes=True):
     # Perform search
     points, boxes = rt.search(radius, origin)
 
     # Plot search
-    rt.plot2D(draw_boxes=True)
+    rt.plot2D(draw_boxes=draw_all_boxes)
     for point in points:
         plt.plot(point[0], point[1], 'go')
-    for box in boxes:
-        rt.plot_box(box, color='r')
+    if draw_searched_boxes:
+        for box in boxes:
+            rt.plot_box(box, color='r')
     plt.gca().add_patch(plt.Circle(origin, radius, color='b', fill=False))
     plt.show()
