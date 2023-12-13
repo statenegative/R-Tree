fa603f8b18f2fed07f969fc7eb78da1024a2dc69
Julia Kaeppel <jm4n1015@gmail.com>
2023-12-12 16:50:25 -0800

    Added query visualization functionality, and fixed a bug with intersection testing.

---
+++ src/search.py
@@ -0,0 +1,23 @@
+# Searches an r-tree.
+import numpy as np
+import numpy.typing as npt
+import random
+import matplotlib.pyplot as plt
+
+from page import Page
+from r_tree import R_Tree
+
+Point = npt.NDArray
+
+def show_search(rt: R_Tree, radius: float, origin: Point):
+    # Perform search
+    points, boxes = rt.search(radius, origin)
+
+    # Plot search
+    rt.plot2D(draw_boxes=True)
+    for point in points:
+        plt.plot(point[0], point[1], 'go')
+    for box in boxes:
+        rt.plot_box(box, color='r')
+    plt.gca().add_patch(plt.Circle(origin, radius, color='b', fill=False))
+    plt.show()
